58fe931e10cb107badccd6226aa3db86be75c4f8
reyhkm <reyhikam04@gmail.com>
2025-08-10 10:38:06 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { fileURLToPath, URL } from "url"
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.url.substring(import.meta.url.indexOf('/') + 1), "./src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 })
